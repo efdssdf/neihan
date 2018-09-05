8a6c0de97fffabd111f957ba5a337514f30de987
efdssdf <efdssdf@gmail.com>
2018-09-05 19:15:32 +0800

    'page'

--- model/Neihan.js
+++ model/Neihan.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 // mongoose.set('debug',true)
 var Schema = mongoose.Schema;
-var connect_url = require('../conf/proj.json').fuwu_mongodb;
+var connect_url = require('../conf/proj.json').mongodb;
 var db = mongoose.createConnection(connect_url);
 
 var NeihanSchema = new Schema({
