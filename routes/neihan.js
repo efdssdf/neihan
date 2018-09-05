79ed8d021b5b7018a8f1001c145d95bacc54dde8
efdssdf <efdssdf@gmail.com>
2018-09-05 19:16:49 +0800

    'page'

--- routes/neihan.js
+++ routes/neihan.js
@@ -4,7 +4,7 @@ var NeihanModel = require('../model/Neihan');
 router.prefix('/neihan');
 
 router.get('/', async(ctx, next) => {
-    let page = ctx.request.query.page
+    let page = ctx.request.query.page || 1
     let messages = await NeihanModel.find().skip(page * 20).limit(20).sort({createAt: -1});
     ctx.body = {messages: messages}
 })
