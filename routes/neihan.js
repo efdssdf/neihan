a46ec3f303af3638ea3184687fa9845dc49d3ce6
efdssdf <efdssdf@gmail.com>
2018-09-07 18:43:36 +0800

    'filter'

--- routes/neihan.js
+++ routes/neihan.js
@@ -5,7 +5,7 @@ router.prefix('/neihan');
 
 router.get('/', async(ctx, next) => {
     let page = ctx.request.query.page || 1
-    let messages = await NeihanModel.find().skip((page-1) * 20).limit(20).sort({createAt: -1});
+    let messages = await NeihanModel.find({source:"neihan"}).skip((page-1) * 20).limit(20).sort({createAt: -1});
     ctx.body = {messages: messages}
 })
 
---
+++ script/filter.js
@@ -0,0 +1,21 @@
+var NeihanModel = require('../model/Neihan');
+var koa2Req = require('koa2-request');
+var schedule = require("node-schedule");
+
+async function filter() {
+    let messages = await NeihanModel.find({createAt: {$lt: Date.now()/1000 - 2 * 3600}})
+    for(var i of messages){
+        var res = await koa2Req(i.thumbnail)
+        if(res.statusCode!=200){
+            await NeihanModel.findByIdAndDelete(i._id)
+        }
+    }
+}
+
+var rule = new schedule.RecurrenceRule();
+var times = [19];
+rule.hour = times;
+var j = schedule.scheduleJob(rule, function () {
+    console.log('过滤图片');
+    filter()
+});
