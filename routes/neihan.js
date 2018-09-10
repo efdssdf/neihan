fcd1d9e25a6434372323ec36682db06035b247bb
efdssdf <efdssdf@gmail.com>
2018-09-10 10:32:37 +0800

    'addId'

--- routes/neihan.js
+++ routes/neihan.js
@@ -5,7 +5,19 @@ router.prefix('/neihan');
 
 router.get('/', async(ctx, next) => {
     let page = ctx.request.query.page || 1
-    let messages = await NeihanModel.find({source:"neihan"}).skip((page-1) * 20).limit(20).sort({createAt: -1});
+    let id = ctx.request.query.id || 0
+    let type = ctx.request.query.type || ""
+    if (type && type == "share") {
+        let messages = await NeihanModel.find({
+            _id: {$gte: id},
+            source: "neihan"
+        }).skip((page - 1) * 20).limit(20).sort({createAt: -1});
+    } else {
+        let messages = await NeihanModel.find({
+            _id: {$gt: id},
+            source: "neihan"
+        }).skip((page - 1) * 20).limit(20).sort({createAt: -1});
+    }
     ctx.body = {messages: messages}
 })
 
