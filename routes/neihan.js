5a8c1f0532660be486c4f647c8560abee3812b10
efdssdf <efdssdf@gmail.com>
2018-09-11 10:20:52 +0800

    'page'

--- routes/neihan.js
+++ routes/neihan.js
@@ -7,22 +7,38 @@ router.get('/', async(ctx, next) => {
     let page = ctx.request.query.page || 1
     let id = ctx.request.query.id
     let type = ctx.request.query.type || ""
+    let arr = [];
     if (type && type == "share") {
         var messages = await NeihanModel.find({
             _id: {$gte: id},
             source: "neihan"
         }).skip((page - 1) * 20).limit(20).sort({createAt: -1});
+        for(var message of messages){
+            let res = JSON.parse(JSON.stringify(message));
+            res.page = page
+            arr.push(res)
+        }
     } else {
         if (id) {
             var messages = await NeihanModel.find({
                 _id: {$gt: id},
                 source: "neihan"
             }).skip((page - 1) * 20).limit(20).sort({createAt: -1});
+            for(var message of messages){
+                let res = JSON.parse(JSON.stringify(message));
+                res.page = page
+                arr.push(res)
+            }
         } else {
             var messages = await NeihanModel.find({source: "neihan"}).skip((page - 1) * 20).limit(20).sort({createAt: -1});
+            for(var message of messages){
+                let res = JSON.parse(JSON.stringify(message));
+                res.page = page
+                arr.push(res)
+            }
         }
     }
-    ctx.body = {messages: messages,version:"1.0.0"}
+    ctx.body = {messages: arr,version:"1.0.0"}
 })
 
 module.exports = router
