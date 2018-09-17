4b06d8146ac96a8f8f92dfea75dd8b3cead65501
efdssdf <efdssdf@gmail.com>
2018-09-17 18:08:42 +0800

    'tenplate'

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const views = require('koa-views')
 const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger') 
+const logger = require('koa-logger')
+const cors = require('koa-cors');
 
 const neihan = require('./routes/neihan')
 const template = require('./routes/template')
@@ -18,6 +19,7 @@ app.use(bodyparser({
 }))
 app.use(json())
 app.use(logger())
+app.use(cors());
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
