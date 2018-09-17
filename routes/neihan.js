cdb7aea00bc2cf79df69afb02465244f6d12da1b
efdssdf <efdssdf@gmail.com>
2018-09-17 17:17:52 +0800

    'ad'

--- routes/neihan.js
+++ routes/neihan.js
@@ -1,5 +1,6 @@
 const router = require('koa-router')()
 var NeihanModel = require('../model/Neihan');
+var ad = require('../conf/ad.json')
 
 router.prefix('/neihan');
 
@@ -16,13 +17,24 @@ router.get('/', async(ctx, next) => {
         arr.push(res[0])
     }
     var messages = await NeihanModel.find({source: {$in:soureArr}}).skip((page - 1) * 20).limit(20).sort({createAt: 1});
-    for (var message of messages) {
-        let res = JSON.parse(JSON.stringify(message));
-        res.page = page
-        arr.push(res)
+    if(messages.length>=20){
+        for (var message of messages) {
+            let res = JSON.parse(JSON.stringify(message));
+            res.page = page
+            arr.push(res)
+        }
+        arr.splice(4,0,ad.ad1)
+        arr.splice(11,0,ad.ad2)
+        arr.splice(18,0,ad.ad3)
+    }else{
+        for (var message of messages) {
+            let res = JSON.parse(JSON.stringify(message));
+            res.page = page
+            arr.push(res)
+        }
+        arr.code = "end"
     }
-    // arr.splite()
-    ctx.body = {messages: arr,length:arr.length, version: "1.0.1"}
+    ctx.body = {messages: arr,code:arr.code, version: "1.0.1"}
 })
 
 module.exports = router
