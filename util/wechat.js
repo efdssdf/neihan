b201d6bf8b96327b81fae0e748a3e92cc45d53bb
efdssdf <efdssdf@gmail.com>
2018-09-18 10:11:02 +0800

    'tenplate'

--- util/wechat.js
+++ util/wechat.js
@@ -20,7 +20,7 @@ async function getAccessToken(code) {
 
 async function getOpenid(code, wxcode) {
     let conf = weichat_conf[code];
-    let url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + conf.appid + "&secret=" + conf.appsecret + "&code=" + wxcode + "&grant_type=authorization_code"
+    let url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + conf.appid + "&secret=" + conf.appsecret + "&code=" + wxcode + "&grant_type=authorization_code&scope=snsapi_base"
     console.log(url,'------------------url')
     let res = await koa2Req(url)
     let data = JSON.parse(res.body)
